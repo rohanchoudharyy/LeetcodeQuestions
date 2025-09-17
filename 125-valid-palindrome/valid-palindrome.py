e2ade61134dd6eeafca64d970b769c4794ba1f3a
Rohan Choudhary <rohanch98@gmail.com>
2025-09-17 15:39:38 +0530

    Time: 29 ms (32.16%) | Memory: 12.6 MB (89.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,14 +1,23 @@
 class Solution(object):
+
     def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        ss = ""
-        for i in s:
-            if i.isalnum():
-                ss+=(i.lower())
-        rev = ss[::-1]
-        if ss!=rev:
-            return False
+        def alnum(string):
+            return ((ord('a') <= ord(string) <= ord('z')) or
+            (ord('A') <= ord(string) <= ord('Z')) or
+            (ord('0') <= ord(string) <= ord('9')))
+
+        s=s.lower()
+        i,j = 0,len(s)-1
+        while(i<j):
+            if not alnum(s[i]):
+                i+=1
+                continue
+            if not alnum(s[j]):
+                j-=1
+                continue
+            if (s[i]!=s[j]):
+                return False
+            i+=1
+            j-=1
+        
         return True
