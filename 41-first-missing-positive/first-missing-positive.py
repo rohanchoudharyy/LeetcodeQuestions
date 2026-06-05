420ab1cd86206979cda1247d8184abaf8e73f50e
Rohan Choudhary <rohanch98@gmail.com>
2026-06-05 19:06:24 +0530

    Time: 18 ms (91.65%) | Memory: 35 MB (13.03%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -2,13 +2,6 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         nums_new = set(nums)
         
-        if len(nums)==1:
-            for i in range(1,len(nums)+1):
-                if 1 in nums_new:
-                    return 2
-                else:
-                    return 1
-
         for i in range(1,len(nums)+1):
             if i not in nums_new:
                 return i
