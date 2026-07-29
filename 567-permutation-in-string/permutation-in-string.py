fff01a175a68262a1fc77304a7be62668b20c909
Rohan Choudhary <rohanch98@gmail.com>
2026-07-29 21:43:51 +0530

    Time: 3052 ms (8.08%) | Memory: 19.5 MB (17.95%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        stringone={}     
+        for char in s1:
+            stringone[char]=stringone.get(char,0)+1
+        
+        i=0
+        j=len(s1)-1
+
+        while j<len(s2):
+            stringtwo={}   
+            window = s2[i:j+1]
+            for char in window:
+                stringtwo[char]=stringtwo.get(char,0)+1
+                if stringone == stringtwo:
+                    return True
+            i+=1
+            j+=1
+        
+        return False
+                
