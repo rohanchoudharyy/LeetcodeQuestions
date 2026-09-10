d20d30ada953d6db8cc9f4bf512d600b962fc5af
Rohan Choudhary <rohanch98@gmail.com>
2026-09-10 13:59:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (12.93%) - LeetSync

--- 682-baseball-game/baseball-game.py
+++ 682-baseball-game/baseball-game.py
@@ -1,15 +1,19 @@
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
-        ops = {'C','+','D'}
-        ans = []
+        ops = {'+','D','C'}
+        stack = []
         for i in operations:
-            if i not in ops:
-                ans.append(int(i))
-            elif i == 'C':
-                ans.pop()
-            elif i == 'D':
-                ans.append(ans[-1]*2)
-            elif i == '+':
-                ans.append(ans[-1]+ans[-2])
+            if (i=='+'):
+                stack.append(stack[-1]+stack[-2])
+            elif (i=='D'):
+                stack.append(stack[-1]*2)
+            elif(i=='C'):
+                stack.pop()
+            else: 
+                stack.append(int(i))
+            
+        sum=0
+        for i in stack:
+            sum+=i
         
-        return sum(ans)
+        return sum
